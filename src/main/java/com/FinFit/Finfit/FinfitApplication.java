030a63c6284c4f8572991f90f542bdc589d108b5
Dario <darioemanuelmarquez@gmail.com>
2021-07-25 11:47:00 -0300

    Created project

---
+++ src/main/java/com/FinFit/Finfit/FinfitApplication.java
@@ -0,0 +1,13 @@
+package com.FinFit.Finfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FinFit/Finfit/FinfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinFit.Finfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
